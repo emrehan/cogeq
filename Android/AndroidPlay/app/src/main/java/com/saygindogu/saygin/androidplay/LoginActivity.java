73f2ce1400438f717a7dc3d3e81640a1db0827c0
saygindogu <saygindogu@gmail.com>
2016-03-05 14:20:38 +0200

    background image is removed because it caused app to crash.

--- Android/AndroidPlay/app/src/main/java/com/saygindogu/saygin/androidplay/LoginActivity.java
+++ Android/AndroidPlay/app/src/main/java/com/saygindogu/saygin/androidplay/LoginActivity.java
@@ -2,6 +2,7 @@ package com.saygindogu.saygin.androidplay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void loginButtonsOnClick( View view){
+        if( view.getId() == R.id.foursquareButton){
+            System.out.println( "Foursquare Button Pressed");
+        }
     }
 }
