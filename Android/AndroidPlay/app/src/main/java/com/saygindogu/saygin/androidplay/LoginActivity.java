4e3df010ec01b0fcf67d31d262aa7335468ab71d
saygindogu <saygindogu@gmail.com>
2016-03-05 14:41:33 +0200

    foursquare dependency is added.

--- Android/AndroidPlay/app/src/main/java/com/saygindogu/saygin/androidplay/LoginActivity.java
+++ Android/AndroidPlay/app/src/main/java/com/saygindogu/saygin/androidplay/LoginActivity.java
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.foursquare.android.nativeoauth.FoursquareOAuth;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String CLIENT_ID = "XWWK4XOYWX3J1XDRN43LL2V0EB41OFCDF3EBZ1CZIABKA1DL";
+    private static final String CLIENT_SECRET = "MGDCFYKO2SZP0TNPWOI4KJ2P5GVHRTWUIAB4O0I25PBH2BAS";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +18,9 @@ public class LoginActivity extends AppCompatActivity {
 
     public void loginButtonsOnClick( View view){
         if( view.getId() == R.id.foursquareButton){
-            System.out.println( "Foursquare Button Pressed");
+            System.out.println("Foursquare Button Pressed");
+            FoursquareOAuth.getConnectIntent( this, CLIENT_ID);
+
         }
     }
 }
