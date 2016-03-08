a212065a9ba578277074711734a668cf2b68a8d7
saygindogu <saygindogu@gmail.com>
2016-03-08 16:55:34 +0200

    settingsDrawer Activity Created

---
+++ Android/AndroidPlay/app/src/main/java/com/cogeq/cogeqapp/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.cogeq.cogeqapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.foursquare.android.nativeoauth.FoursquareOAuth;
+import com.foursquare.android.nativeoauth.model.AuthCodeResponse;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "XWWK4XOYWX3J1XDRN43LL2V0EB41OFCDF3EBZ1CZIABKA1DL";
+    private static final String CLIENT_SECRET = "MGDCFYKO2SZP0TNPWOI4KJ2P5GVHRTWUIAB4O0I25PBH2BAS";
+    private static final int REQUEST_CODE_FSQ_CONNECT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginButtonsOnClick( View view){
+        if( view.getId() == R.id.foursquareButton){
+            System.out.println("Foursquare Button Pressed");
+            Intent intent = FoursquareOAuth.getConnectIntent( this, CLIENT_ID);
+            startActivityForResult(intent, REQUEST_CODE_FSQ_CONNECT);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_FSQ_CONNECT:
+                AuthCodeResponse codeResponse = FoursquareOAuth.getAuthCodeFromResult(resultCode, data);
+                /* ... */
+                codeResponse.getException();
+                System.out.println( "Code:" + codeResponse.getCode());
+                break;
+        }
+    }
+
+    public void signUpButtonPressed( View view){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity( intent);
+    }
+}
---
+++ Android/AndroidPlay/app/src/main/java/com/cogeq/cogeqapp/PreferenceSelectActivity.java
@@ -0,0 +1,50 @@
+package com.cogeq.cogeqapp;
+import java.util.ArrayList;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class PreferenceSelectActivity extends ListActivity {
+
+    private ProgressDialog m_ProgressDialog = null;
+    private ArrayList<TravelPreference> m_preferences = null;
+    private PreferenceAdapter m_adapter;
+    private Runnable viewOrders;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference);
+
+        m_preferences = new ArrayList<>();
+        m_preferences.add( new TravelPreference("beach", R.drawable.beach));
+        m_preferences.add( new TravelPreference("backpack", R.drawable.backpack));
+        m_preferences.add( new TravelPreference("museum", R.drawable.museum));
+        m_preferences.add( new TravelPreference("economy", R.drawable.economy));
+        m_preferences.add( new TravelPreference("nature", R.drawable.nature));
+        m_preferences.add( new TravelPreference("shopping", R.drawable.shopping));
+
+        m_adapter = new PreferenceAdapter(this, R.layout.row, m_preferences);
+        setListAdapter(m_adapter);
+        //Ben Can
+    }
+
+    public void preferenceOnClick(View view){
+        ListView lv = getListView();
+        int pos = lv.getPositionForView( view);
+
+        boolean isSelected = m_preferences.get(pos).isSelected();
+        if( isSelected){
+            view.findViewById(R.id.icon).setVisibility(View.INVISIBLE);
+            m_preferences.get(pos).setIsSelected(false);
+        }
+        else {
+            view.findViewById(R.id.icon).setVisibility(View.VISIBLE);
+            m_preferences.get(pos).setIsSelected( true);
+        }
+    }
+}
