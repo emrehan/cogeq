067358da44af49003cb377ed9938def8ac55e3cd
emrehan <emrehan.tuzun@gmail.com>
2016-03-27 13:11:27 +0300

    Move Google Places API key to config.json

--- Backend/server.py
+++ Backend/server.py
@@ -3,6 +3,7 @@ import requests
 import json
 app = Flask(__name__)
 
+config = json.load(open('config.json'))
 
 @app.route("/", methods=['GET'])
 def hello():
@@ -12,14 +13,14 @@ def hello():
 def search_cities():
     try:
         query = request.args.get('query')
-        r = requests.get('https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + query + '&types=(cities)&key=AIzaSyDbpTvwo005xWG0rGdvaFLCLX-GE-phsQA')
+        r = requests.get('https://maps.googleapis.com/maps/api/place/autocomplete/json?types=(cities)&key=' + config['keys']['google_places'] + '&input=' + query )
         
         if (r.status_code == 200):     
             cities = list( map( lambda x: x['description'], r.json()['predictions'] ) )
             return jsonify( {'cities': cities} )
         else:
             return jsonify( {'Error': 'Error while getting Google Place API response'} )
-    except:
+    except KeyError:
         default_cities = ['Istanbul, Turkey', 'London, United Kingdom', 'Izmir, Turkey', 'Singapore, Singapore', 'NYC, United States']
         return jsonify( {'cities': default_cities} )
         
