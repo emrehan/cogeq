7f9a0b45e8e544176811785ea31b12594ae7965c
saygindogu <saygindogu@gmail.com>
2016-03-27 13:21:18 +0200

    Search City is implemented.

---
+++ Android/SmartTravelling(COGEQ)/app/src/main/java/com/cogeq/cogeqapp/ArrayAdapterSearchView.java
@@ -0,0 +1,50 @@
+package com.cogeq.cogeqapp;
+
+import android.content.Context;
+import android.support.v7.widget.SearchView;
+import android.util.AttributeSet;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+
+
+
+/**
+ * Created by saygin on 3/27/2016.
+ */
+public class ArrayAdapterSearchView extends SearchView {
+
+    private SearchView.SearchAutoComplete mSearchAutoComplete;
+
+    public ArrayAdapterSearchView(Context context) {
+        super(context);
+        initialize();
+    }
+
+    public ArrayAdapterSearchView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize();
+    }
+
+    public void initialize() {
+        mSearchAutoComplete = (android.support.v7.widget.SearchView.SearchAutoComplete) findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        this.setAdapter(null);
+        this.setOnItemClickListener(null);
+    }
+
+    public void setSuggestionsAdapter(CursorAdapter adapter) {
+        // don't let anyone touch this
+    }
+
+    public void setOnItemClickListener(AdapterView.OnItemClickListener listener) {
+        mSearchAutoComplete.setOnItemClickListener(listener);
+    }
+
+    public void setAdapter(ArrayAdapter<?> adapter) {
+        mSearchAutoComplete.setAdapter(adapter);
+    }
+
+    public void setText(String text) {
+        mSearchAutoComplete.setText(text);
+    }
+}
--- Android/SmartTravelling(COGEQ)/app/src/main/java/com/cogeq/cogeqapp/SearchCityFragment.java
+++ Android/SmartTravelling(COGEQ)/app/src/main/java/com/cogeq/cogeqapp/SearchCityFragment.java
@@ -6,15 +6,113 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by Ratan on 7/29/2015.
  */
 public class SearchCityFragment extends Fragment {
 
+    String[] temp = { "Ankara", "Izmir", "Istanbul" , "Antalya", "Gaziantep"};
+    private ListView searchList;
+    private ArrayAdapter<String> adapter;
+    private SearchView searchView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.city_layout,null);
+        View view = inflater.inflate(R.layout.city_layout,null);
+        adapter = new ArrayAdapter<String>( getContext(), R.layout.adapter_resource, temp );
+        searchList = (ListView)view.findViewById( R.id.search_list);
+        searchView = (SearchView) view.findViewById( R.id.searchView);
+        searchList.setAdapter( adapter);
+
+        searchList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                TextView textView = (TextView) searchList.getItemAtPosition(position);//TODO: class cast exception
+                searchView.setQuery( textView.getText(), false);
+            }
+        });
+
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchList.setVisibility(View.INVISIBLE);
+                System.out.println( "Query:" + query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText != null) {
+                    searchList.setVisibility(View.VISIBLE);
+                } else {
+                    searchList.setVisibility(View.INVISIBLE);
+                }
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue( getContext());
+                String url = getString( R.string.backendServer ) + "/cities?query=" + newText;
+                // Request a string response from the provided URL.
+                System.out.println( "URL:" + url);
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                // Display the first 500 characters of the response string.
+                                //TODO: parse json
+                                try {
+                                    //JSONObject jsonObject = new JSONObject(response);
+                                    JSONObject jsonObject = response;
+                                    JSONArray jsonArray = jsonObject.getJSONArray("cities");
+                                    ArrayList<String> list = new ArrayList<String>();
+                                    if (jsonArray != null) {
+                                        int len = jsonArray.length();
+                                        for (int i=0;i<len;i++){
+                                            list.add(jsonArray.get(i).toString());
+                                        }
+                                    }
+                                    adapter = new ArrayAdapter<String>( getContext(), R.layout.adapter_resource , list );
+                                    searchList.setAdapter( adapter);
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println( "Connection Error!");
+                    }
+                });
+
+                // Add the request to the RequestQueue.
+                queue.add(jsonObjectRequest);
+                //adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return view;
     }
 }
