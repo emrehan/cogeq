6d9bb8b654d3b41525f8ac99e6c645af9dddf8cc
emrehan <emrehan.tuzun@gmail.com>
2016-03-27 16:40:25 +0300

    Integrate MongoDB

--- Backend/server.py
+++ Backend/server.py
@@ -1,13 +1,30 @@
 from flask import Flask, request, jsonify
 import requests 
 import json
-app = Flask(__name__)
+from flask.ext.pymongo import PyMongo
+from bson import Binary, Code
+from bson.json_util import dumps
 
+app = Flask(__name__)
+mongo = PyMongo(app)
 config = json.load(open('config.json'))
 
 @app.route("/", methods=['GET'])
 def hello():
     return "COGEQ!"
+
+@app.route("/db", methods=['GET', 'POST', 'DELETE'])
+def dummy_db():
+    if request.method == 'GET':
+        return dumps( {'travels': list( mongo.db.travels.find() )} )
+    if request.method == 'POST':
+        travel = {'city': 'Istanbul, Turkey'}
+        travel_id = mongo.db.travels.insert_one(travel).inserted_id
+        print (travel_id)
+        return dumps( {'id': travel_id} )
+    if request.method == 'DELETE':
+        r = mongo.db.travels.delete_many({})
+        return dumps( {'deleted_count': r.deleted_count } )
 
 @app.route("/cities", methods=['GET'])
 def search_cities():
@@ -17,12 +34,12 @@ def search_cities():
         r = requests.get('https://maps.googleapis.com/maps/api/place/autocomplete/json?types=(cities)&key=' + config['keys']['google_places'] + '&input=' + query )        
         if (r.status_code == 200):     
             cities = list( map( lambda x: x['description'], r.json()['predictions'] ) )
-            return json.dumps( {'cities': cities}, ensure_ascii=False )
+            return dumps( {'cities': cities}, ensure_ascii=False )
         else:
-            return json.dumps( {'Error': 'Error while getting Google Place API response'}, ensure_ascii=False )
+            return dumps( {'Error': 'Error while getting Google Place API response'}, ensure_ascii=False )
     else:
         default_cities = ['Istanbul, Turkey', 'London, United Kingdom', 'Izmir, Turkey', 'Singapore, Singapore', 'NYC, United States']
-        return json.dumps( {'cities': default_cities}, ensure_ascii=False)
+        return dumps( {'cities': default_cities}, ensure_ascii=False)
         
 @app.route("/travels", methods=['GET'])
 def create_travel():
@@ -31,13 +48,13 @@ def create_travel():
 		ffrom = datetime.strptime(request.args.get('from'), '%Y-%m-%dT%H:%M:%S')
 		to = datetime.strptime(request.args.get('to'), '%Y-%m-%dT%H:%M:%S')
 		if (not city or not ffrom or not to):
-			return jsonify( {'Unexpected Error': 'city, from and to must be provided.'} )
+			return dumps( {'Error': 'city, from and to must be provided.'} )
 		else:
 			travel_id = uuid.uuid1()
 			activities = []
-			return jsonify( {'travel_id': travel_id, 'from': ffrom, 'to': to, 'activities': activities })
+			return dumps( {'travel_id': travel_id, 'from': ffrom, 'to': to, 'activities': activities })
 	except:
-		return jsonify({'Unexpected Error': 'Unexpected Error occured'})
+		return dumps({'Error': 'Error occured'})
 
 if __name__ == "__main__":
     app.debug = True
--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 ###### The badass senior project.
 
+ 
+## Installing MongoDB
+ 
+According to the [MongoDB](http://mongodb.org/) installation instructions for [Linux](https://docs.mongodb.org/master/administration/install-on-linux/), [OS X](https://docs.mongodb.org/master/tutorial/install-mongodb-on-os-x/) and [Windows](https://docs.mongodb.org/master/tutorial/install-mongodb-on-windows/).
 
-# Running Server
+## Running MongoDB
+
+
+## Running Server
 ```
 pip3 install -r requirements.txt
 python3 server.py
