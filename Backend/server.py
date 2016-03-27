a9b457701ec2acf12c575d838c17c937160d8555
emrehan <emrehan.tuzun@gmail.com>
2016-03-27 13:31:40 +0300

    Improve cities endpoint

--- Backend/server.py
+++ Backend/server.py
@@ -11,18 +11,19 @@ def hello():
 
 @app.route("/cities", methods=['GET'])
 def search_cities():
-    try:
-        query = request.args.get('query')
-        r = requests.get('https://maps.googleapis.com/maps/api/place/autocomplete/json?types=(cities)&key=' + config['keys']['google_places'] + '&input=' + query )
-        
+    query = request.args.get('query')
+
+    if query:
+        r = requests.get('https://maps.googleapis.com/maps/api/place/autocomplete/json?types=(cities)&key=' + config['keys']['google_places'] + '&input=' + query )        
         if (r.status_code == 200):     
             cities = list( map( lambda x: x['description'], r.json()['predictions'] ) )
             return jsonify( {'cities': cities} )
         else:
             return jsonify( {'Error': 'Error while getting Google Place API response'} )
-    except KeyError:
+    else:
         default_cities = ['Istanbul, Turkey', 'London, United Kingdom', 'Izmir, Turkey', 'Singapore, Singapore', 'NYC, United States']
         return jsonify( {'cities': default_cities} )
         
 if __name__ == "__main__":
+    app.debug = True
     app.run()
