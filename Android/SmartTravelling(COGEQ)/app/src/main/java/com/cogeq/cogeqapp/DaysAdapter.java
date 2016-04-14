24a943897954e80594100b9285f55c6e803d20dd
saygindogu <saygindogu@gmail.com>
2016-04-14 16:51:59 +0200

    bug with the single date select is fixed.

--- Android/SmartTravelling(COGEQ)/app/src/main/java/com/cogeq/cogeqapp/DaysAdapter.java
+++ Android/SmartTravelling(COGEQ)/app/src/main/java/com/cogeq/cogeqapp/DaysAdapter.java
@@ -1,10 +1,12 @@
 package com.cogeq.cogeqapp;
 
 import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,11 +30,20 @@ public class DaysAdapter extends ArrayAdapter<DaysObject> {
                 LayoutInflater vi = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 v = vi.inflate(R.layout.row_of_days, null);
             }
-            DaysObject days = items.get(position);
-            if (days != null) {
+            DaysObject daysObject = items.get(position);
+            if (daysObject != null) {
                 TextView tt = (TextView) v.findViewById(R.id.daysOfRows);
                 if (tt != null) {
-                    tt.setText(days.getDaysName());
+                    tt.setText(daysObject.getDaysName());
+                }
+                ImageView iv = (ImageView) v.findViewById(R.id.daysTick);
+                if( iv != null){
+                    if( daysObject.isSelected() ){
+                        iv.setVisibility( View.VISIBLE);
+                    }
+                    else{
+                        iv.setVisibility( View.INVISIBLE);
+                    }
                 }
             }
             return v;
